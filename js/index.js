b4f99dfa7e479ae783186e8c6007684203aef3e1
Anica <anicagabriellips@gmail.com>
2025-02-03 10:45:00 -0300

    concertando erro de espaçamento

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ titulo.innerText = "Mulheres Negras em Guanambi: Superando a Invisibilidade";
 
 textoIntroducao.innerText = "Descubra a força da representatividade feminina! Nosso Projeto Integrador visa destacar e celebrar as mulheres negras líderes em Guanambi. Explore conosco como suas trajetórias e realizações inspiram mudanças e promovem a igualdade de gênero e raça. Junte-se a nós nessa jornada de reconhecimento e visibilidade. Juntas, podemos construir um futuro mais inclusivo e diversificado!";
 
-iframe.setAttribute("height", "315");
+iframe.setAttribute("height", "100");
 iframe.setAttribute("src", "https://www.youtube.com/embed/BwCGXExjFyI?si=8aPAVrDOhnJumx9o");
 iframe.setAttribute("title", "YouTube video player");
 iframe.setAttribute("frameborder", "0");
